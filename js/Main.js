429eb7750bafa45b5d1600f0d2c83c5ebde0db2e
villain2 <djvillain@hotmail.com>
2013-12-22 19:45:45 -0500

    new images and js

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,9 @@
 var ddApp = angular.module("ddApp", []);
 
+ddApp.factory('Data', function () {
+	return {message: "I'm data from a service."};
+});
+
 function mainCtrl($scope, Data) {
     $scope.data     = Data;
     console.log($scope);
