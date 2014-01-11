59ea9268f55993cf45ae998b22e0cf8fe06dc871
villain2 <djvillain@hotmail.com>
2014-01-10 19:09:55 -0500

    updating

--- game/app/js/app.js
+++ game/app/js/app.js
@@ -9,8 +9,19 @@ angular.module('myApp', [
   'myApp.directives',
   'myApp.controllers'
 ]).
+run(['$rootScope', function($rootScope) {
+    //set global variables
+    $rootScope.gameField        = [];
+    $rootScope.stage            = new createjs.Stage("gameGrid");
+    $rootScope.actorContainer;
+    $rootScope.actor;
+    $rootScope.movingActor;
+    $rootScope.actorMoving      = false;
+    $rootScope.selector;
+}]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/chapter1_1.html', controller: 'Level1Ctrl'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/view1', {templateUrl: 'partials/level1.html', controller: 'Level1Ctrl'});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.otherwise({redirectTo: '/view1'});
+    
 }]);
--- game/app/js/controllers.js
+++ game/app/js/controllers.js
@@ -1,35 +1,52 @@
 'use strict';
 
 /* Controllers */
-
 angular.module('myApp.controllers', []).
+controller('Level1Ctrl', ['$scope', '$rootScope', '$window', function($scope, $rootScope, $window) {
+    setupField();
+    drawField();
+    drawCharacter();
 
-controller('Level1Ctrl', [function () {
-	var stage		= new createjs.Stage("gameGrid");
-
-	var circle		= new createjs.Shape();
-	circle.graphics.beginFill('red').drawCircle(0,0,50);
-	circle.x		= 100;
-	circle.y		= 100;
-	stage.addChild(circle);
-	setupField();
-	stage.update();
+    function setupField() {
+        $rootScope.gameField           = new Array();
+        for(var i=0; i < 5; i++) {
+            $rootScope.gameField[i]        = new Array();
+            for(var j=0; j < 9; j++) {
+                $rootScope.gameField[i][j]=0;
+            }
+        }
+    }
+    
+    function drawField() {
+        var fieldSprite         = new createjs.Shape();
+        var randomGreen;
+        var stage               = $rootScope.stage;
+        stage                   = new createjs.Stage("gameGrid");
+        stage.addChild(fieldSprite);
+        
+        fieldSprite.graphics.setStrokeStyle(1).beginStroke('#ffffff');
+        
+        for(var i = 0; i < 5; i++) {
+            for (var j = 0; j < 9; j++) {
+                randomGreen = (125+Math.floor(Math.random()*50))*256;
+                fieldSprite.graphics.beginFill('#' + randomGreen + '0').drawRect(25+65*j, 80+75*i, 65, 75);
+            }
+        }
+        
+        stage.update();
+    }
+    
 }])
+.controller('MyCtrl2', ['$scope', function($scope) {
 
-  .controller('MyCtrl1', [function() {
+}]);
 
-  }])
-  .controller('MyCtrl2', [function() {
+var GameController = function ($scope) {
+    console.log('okay');
+}
 
-  }]);
-
-  function setupField() {
-  	var gameField			= new Array();
-  	for(var i = 0; i < 5; i++) {
-  		gameField[i] = new Array();
-  		for(var j=0; j < 9; j++) {
-  			gameField[i][j] = 0;
-  		}
-  	}
-  	console.log(gameField);
-  }
+function drawCharacter() {
+	var actor;
+	actor = "warrior";
+	return actor;
+}
